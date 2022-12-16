69f6c5647be9956ec0e20e3fb51ae3d48daec2cd
thulanimbatha <thulanimbatha06@gmail.com>
2022-12-17 01:01:18 +0200

    version 2.0

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')  # added kwarg
-    password = PasswordField(label='Password')    # changed to Password field
+    email = StringField(label='Email', validators=[DataRequired(), Email(message="Please enter valid email!")]) # validators
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, message="Password needs 8 or more characters")])    
     submit = SubmitField(label='Log In')
 
 app = Flask(__name__)
@@ -14,9 +15,14 @@ app.secret_key = "IShouldBeAlseepByNow15"
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])   # respond to POST requests
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit(): # validate on submit
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=login_form)
 
 
